61e5d0a95bbc423fd6d432e7bdaf6cad51639526
tomignzlz <tomig3a@gmail.com>
2025-09-17 21:23:50 -0300

    Ejercicio del metodo Monte Carlo temp.py

--- temp.py
+++ temp.py
@@ -32,6 +32,42 @@ q = 2
 X = [np.array([1,2,3]),np.array([3,3,3]),np.array([1,1,1])]
 print(normaliza(X,q))
 
+def normaMatMC(A, q, p, Np):
+   
+    n = A.shape[1]
+    mejorvalor = 0
+    mejorvector = " "
+
+    for _ in range(Np):
+       
+        x = np.random.randn(n) #genero los vectores aleatorios (los np)
+
+ 
+        normadex = norma(x, p)
+        if normadex != 0:
+            for i in range(n):
+                x[i] = x[i] / normadex
+
+        
+            Ax = np.dot(A, x) #me calcula la norma "q" Ax
+            val = norma(Ax, q)
+
+            if val > mejorvalor:
+                mejorvalor = val
+                mejorvector = np.array(x) #actualizo el mejor
+
+    return mejorvalor, mejorvector
+
+
+A = np.array([[1, 2],
+              [3, 4]], dtype=float)
+
+
+valor, xopt = normaMatMC(A, q=2, p=2, Np=5)
+
+print("Norma inducida estimada:", valor)
+print("Vector óptimo aproximado:", xopt)
+
 
 
 
